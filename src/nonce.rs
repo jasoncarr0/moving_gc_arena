ebb2d5a9d4efdddf919382a86a26fc754333e887
Jason Carr <jcarr250@gmail.com>
2020-01-11 17:41:22 -0500

    Fix file missing in git for debug-arena

---
+++ src/nonce.rs
@@ -0,0 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+use std::sync::atomic::{AtomicU64, Ordering};
+
+static NONCE: AtomicU64 = AtomicU64::new(0);
+
+pub(crate) fn next() -> u64 {
+    NONCE.fetch_add(1, Ordering::AcqRel)
+}
