5eb50ef7968e6fdb374ed4e8c767fd4ffb1920cd
Jason Carr <jcarr250@gmail.com>
2020-01-22 17:16:05 -0500

    Delete some commented flags

--- src/entry.rs
+++ src/entry.rs
@@ -1,12 +1,4 @@
 
-//#[cfg(not (feature = "header_safety"))]
-//mod safe_entry;
-
-//#[cfg(not (feature = "header_safety"))]
-//pub use safe_entry::*;
-
-//#[cfg(feature = "header_safety")]
-//#[cfg(feature = "header_safety")]
 #[allow(unused)]
 mod unsafe_entry;
 #[allow(unused)]
